8a37b53c79ec453fb5e944278b69bce9127fddfa
hanfinetree <kucuhul@gmail.com>
2026-01-20 09:19:01 +0900

    updates

--- Provisioning/4_Install_OpenHPC_Development_Components_4.0.sh
+++ Provisioning/4_Install_OpenHPC_Development_Components_4.0.sh
@@ -7,60 +7,59 @@ echo
 # 4.1 Development Tools
 # Install autotools meta-package (Default)
 echo "##### Start... Install autotools meta-package (Default) "
-yum -y install  ohpc-autotools EasyBuild-ohpc hwloc-ohpc spack-ohpc valgrind-ohpc \
+dnf -y install  ohpc-autotools EasyBuild-ohpc hwloc-ohpc spack-ohpc valgrind-ohpc \
 >> ~/dasan_log_ohpc_autotools,meta-package.txt 2>&1
 tail -1 ~/dasan_log_ohpc_autotools,meta-package.txt
 echo
 echo
 # 4.2 Compilers 
 echo "##### Start... Install Compilers"
-yum -y install  gnu14-compilers-ohpc >> ~/dasan_log_ohpc_Compilers.txt 2>&1
+dnf -y install  gnu15-compilers-ohpc >> ~/dasan_log_ohpc_Compilers.txt 2>&1
 tail -1 ~/dasan_log_ohpc_Compilers.txt
 echo
 echo
 # 4.3 MPI Stacks
 echo "##### Start... Install MPI Stacks"
-yum -y install  openmpi5-pmix-gnu14-ohpc mpich-ofi-gnu14-ohpc mpich-ucx-gnu14-ohpc mvapich2-gnu14-ohpc \ 
->> ~/dasan_log_ohpc_MPI-Stacks.txt 2>&1
+dnf -y install  openmpi5-pmix-gnu15-ohpc mpich-ofi-gnu15-ohpc mpich-ucx-gnu15-ohpc mvapich2-gnu15-ohpc \
+ mvapich2-psm2-gnu15-ohpc >> ~/dasan_log_ohpc_MPI-Stacks.txt 2>&1
 tail -1 ~/dasan_log_ohpc_MPI-Stacks.txt
 echo
 echo
 # 4.4 Performance Tools
 # Install perf-tools meta-package
 echo "##### Start... Install perf-tools meta-package "
-yum -y install  ohpc-gnu14-perf-tools  ohpc-gnu14-geopm \
- >> ~/dasan_log_ohpc_perf-tools.txt 2>&1
+dnf -y install  ohpc-gnu15-perf-tools  >> ~/dasan_log_ohpc_perf-tools.txt 2>&1
 tail -1 ~/dasan_log_ohpc_perf-tools.txt
 echo
 echo
 # 4.5 Setup default development environment
 echo "##### Start... Install default development environment"
-yum -y install   lmod-defaults-gnu14-openmpi5-ohpc  >> ~/dasan_log_ohpc_lmod.txt 2>&1
+dnf -y install  lmod-defaults-gnu15-openmpi5-ohpc  >> ~/dasan_log_ohpc_lmod.txt 2>&1
 tail -1 ~/dasan_log_ohpc_lmod.txt
 echo
 echo
 # 4.6 3rd Party Libraries and Tools
 # Install 3rd party libraries/tools meta-packages built with GNU toolchain
 echo "##### Start... Install 3rd Party Libraries and Tools"
-yum -y install  ohpc-gnu14-serial-libs ohpc-gnu14-io-libs ohpc-gnu14-python-libs \
- ohpc-gnu14-runtimes >> ~/dasan_log_ohpc_3rdPartyLib.txt 2>&1
+dnf -y install  ohpc-gnu15-serial-libs ohpc-gnu15-io-libs ohpc-gnu15-python-libs \
+ ohpc-gnu15-runtimes >> ~/davsan_log_ohpc_3rdPartyLib.txt 2>&1
 tail -1 ~/dasan_log_ohpc_3rdPartyLib.txt
 echo
 echo
 # Install parallel lib meta-packages for all available MPI toolchains
 echo "##### Start... Install parallel lib meta-packages for all available MPI toolchains"
-yum -y install  ohpc-gnu14-mpich-parallel-libs ohpc-gnu14-openmpi5-parallel-libs \
+dnf -y install  ohpc-gnu15-mpich-parallel-libs ohpc-gnu15-openmpi5-parallel-libs \
   >> ~/dasan_log_ohpc_parallellib.txt 2>&1
 tail -1 ~/dasan_log_ohpc_parallellib.txt
 echo
 echo
 # Enable Intel oneAPI and install OpenHPC compatibility packages
 echo "##### Enable Intel oneAPI and install OpenHPC compatibility packages"
-yum -y install intel-oneapi-toolkit-release-ohpc   >> ~/dasan_log_ohpc_inteloneapi.txt 2>&1
+dnf -y install intel-oneapi-toolkit-release-ohpc   >> ~/dasan_log_ohpc_inteloneapi.txt 2>&1
 
 rpm --import https://yum.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB
 
-yum -y install intel-compilers-devel-ohpc intel-mpi-devel-ohpc\
+dnf -y install intel-compilers-devel-ohpc intel-mpi-devel-ohpc\
   >> ~/dasan_log_ohpc_inteloneapi.txt 2>&1
 
 tail -1 ~/dasan_log_ohpc_inteloneapi.txt
@@ -68,7 +67,7 @@ echo
 echo
 # Install 3rd party libraries/tools meta-packages built with Intel toolchain
 echo "##### Install 3rd party libraries/tools meta-packages built with Intel toolchain"
-yum -y install                     \
+dnf -y install                     \
  openmpi5-pmix-intel-ohpc          \
  ohpc-intel-serial-libs            \
  ohpc-intel-geopm                  \
